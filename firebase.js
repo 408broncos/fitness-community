11310bb9c2b0c18bfea605b421b01278843c9fbf
Austin Zumbro <azumbro@gmail.com>
2023-04-11 10:39:22 -0700

    refine signup login

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-analytics.js";
-// import { getDatabase } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,15 +18,16 @@ const firebaseConfig = {
     messagingSenderId: "511898627716",
     appId: "1:511898627716:web:1d92042d5e9ab14d635cd1",
     measurementId: "G-HBWMM2ZTMJ",
-    // databaseURL: "https://fitness-community-bbfba-default-rtdb.firebaseio.com/",
+    databaseURL: "https://fitness-community-bbfba-default-rtdb.firebaseio.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
 const signup = document.getElementById("sign-up");
-const login = document.getElementById("login")
+const login = document.getElementById("login");
 
 
 // Handle the login/sign-up screen forms.
@@ -39,11 +41,13 @@ function signupLoginHandler(event) {
     if (element.id == "sign-up") {
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                // Signed in 
+                // Sign up successful
                 const user = userCredential.user;
                 console.log("Sign Up Success!")
                 console.log(user);
                 // ...
+                document.location = "home.html";
+
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -60,6 +64,7 @@ function signupLoginHandler(event) {
                 console.log("Login Success!")
                 console.log(user);
                 // ...
+                document.location = "home.html";
             })
             .catch((error) => {
                 const errorCode = error.code;
