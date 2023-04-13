df1355c8e0a5f3e9f16311f422e2ca67a3f25885
Ss0110 <saraysanchez01@yahoo.com>
2023-04-13 13:02:21 -0700

    aqi working

--- assets/aqi.js
+++ assets/aqi.js
@@ -1,5 +1,23 @@
-const iqAir = "1c89bd54-fc84-44cd-adb6-a840d493e162";
+// const iqAir = "1c89bd54-fc84-44cd-adb6-a840d493e162";
+// const iqAirImage ="https://www.airvisual.com/images/";
+// function getIQAirData(city, state, country) {
+//     let url = "https://api.airvisual.com/v2/city?city=" + city + "&state=" + state + "&country=" + country + "&key=" + iqAir;
+//     fetch(url).then(function(response){
+//         return response.json()
 
-function getIQAirData(city, state, country) {
-    let url = "https://api.airvisual.com/v2/city?city=" + city + "&state=" + state + "&country=" + country + "&key=" + iqAir;
-}
+//     }).then(function(data){
+//         let aqius = data.data.current.pollution.aqius
+//         let icon = data.data.current.weather.ic
+//         updateAQI(aqius, icon)
+//     })
+     
+// }
+// function updateAQI(aqius, icon){
+//     let aqidiv = document.createElement("div")
+//     aqidiv.textContent = "AQI: " + aqius;
+//     let iconurl = iqAirImage + icon + ".png";
+//     let iconimg = document.createElement("img")
+//     iconimg.setAttribute("src", iconurl)
+// }
+// getIQAirData("San Jose", "California", "USA")
+
--- assets/workout.js
+++ assets/workout.js
@@ -112,6 +112,42 @@ function addExerciseToSchedule(name, reps, desc) {
     constructWorkoutSection();
 }
 
+
+
+function updateAQI(aqius, icon){
+    let aqidiv = document.createElement("div")
+    aqidiv.textContent = "AQI: " + aqius;
+    let iconurl = iqAirImage + icon + ".png";
+    let iconimg = document.createElement("img")
+    iconimg.setAttribute("src", iconurl)
+    let currentday = dayjs().day()
+    let currentdayid = "day-" + currentday
+    let currentdaybox = document.getElementById(currentdayid)
+    currentdaybox.appendChild(aqidiv)
+    currentdaybox.appendChild(iconimg)
+
+}
+
+const iqAir = "1c89bd54-fc84-44cd-adb6-a840d493e162";
+const iqAirImage ="https://www.airvisual.com/images/";
+function getIQAirData(city, state, country) {
+    let url = "https://api.airvisual.com/v2/city?city=" + city + "&state=" + state + "&country=" + country + "&key=" + iqAir;
+    fetch(url).then(function(response){
+        return response.json()
+
+    }).then(function(data){
+        let aqius = data.data.current.pollution.aqius
+        let icon = data.data.current.weather.ic
+        updateAQI(aqius, icon)
+    })
+     
+}
+
+
+
+
+
+
 //form submit section
 var formEl = $('add-exercise');
 var nameEl = $('input[name="exercise-name"]');
@@ -294,6 +330,9 @@ function init() {
     newIdeasButtonArea.addEventListener("click", popupNewExerciseDetails);
     scheduleDays.addEventListener("click", changeActiveDay);
     workoutSection.addEventListener("click", popupListedExercise)
+    getIQAirData("San Jose", "California", "USA")
 }
 
 init();
+
+
